c868abc021219bf2e9adc273b783d173e623409f
Ortega Florencia <florortega055@gmail.com>
2024-07-03 11:06:40 -0300

    Iniciando proyecto

---
+++ src/pages/CrearVideo/index.jsx
@@ -0,0 +1,9 @@
+import styles from './CrearVideo.module.css'
+
+const CrearVideo = () => {
+  return (
+    <div>CrearVideo</div>
+  )
+}
+
+export default CrearVideo
---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar";
+import styles from "./Inicio.module.css";
+
+const Inicio = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default Inicio;
